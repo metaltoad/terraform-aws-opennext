8589a0c3807d28047eb005edccd45760256161d8
BrunoNdeSousa <brunonascimentonewp@gmail.com>
2025-09-29 19:00:55 -0300

    Update sqs.tf
    Signed-off-by: BrunoNdeSousa <brunonascimentonewp@gmail.com>

--- modules/opennext-revalidation-queue/sqs.tf
+++ modules/opennext-revalidation-queue/sqs.tf
@@ -8,6 +8,7 @@ resource "aws_sqs_queue" "revalidation_queue" {
   content_based_deduplication       = true
   kms_master_key_id                 = local.revalidation_kms_key_arn
   kms_data_key_reuse_period_seconds = 300
+  visibility_timeout_seconds        = 360
 }
 
 resource "aws_lambda_event_source_mapping" "revalidation_queue_source" {
